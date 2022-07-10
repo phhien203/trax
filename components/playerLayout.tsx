2d0966a42e7577f993cdf868dbc887fb6e14627b
Pham Huu Hien <phhien203@gmail.com>
2022-07-10 21:42:42 +0700

    feat(PlayerLayout): setup layout container

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -2,9 +2,16 @@ import { Box } from "@chakra-ui/layout";
 
 const PlayerLayout = ({ children }) => {
   return (
-    <Box>
-      PlayerLayout works
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" left="0" width="250px">
+        <h2>Sidebar Section</h2>
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" bottom="0" left="0">
+        <h2>Player Section</h2>
+      </Box>
     </Box>
   );
 };
