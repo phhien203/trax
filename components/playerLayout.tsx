258ba77802da2302cb11480c4a696244abea0705
Pham Huu Hien <phhien203@gmail.com>
2022-07-10 21:25:20 +0700

    feat(PlayerLayout): create PlayerLayout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      PlayerLayout works
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
