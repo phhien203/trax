44972a6cc40fb0b941342dd13e447c28abb42cf0
Pham Huu Hien <phhien203@gmail.com>
2022-07-11 09:35:00 +0700

    feat(PlayerLayout): build the sidebar layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" left="0" width="250px">
-        <h2>Sidebar Section</h2>
+        <Sidebar />
       </Box>
+
       <Box marginLeft="250px" marginBottom="100px">
         {children}
       </Box>
+
       <Box position="absolute" bottom="0" left="0">
         <h2>Player Section</h2>
       </Box>
